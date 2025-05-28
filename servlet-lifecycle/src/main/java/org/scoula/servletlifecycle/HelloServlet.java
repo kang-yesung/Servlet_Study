725400dbd5c41010dee7105721c578ee3fd7e802
kang-yesung <rkddptjd5228@gmail.com>
2025-05-28 16:25:50 +0900

    servlet 기초 실습

---
+++ servlet-lifecycle/src/main/java/org/scoula/servletlifecycle/HelloServlet.java
@@ -0,0 +1,27 @@
+package org.scoula.servletlifecycle;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "서블릿 입문!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charSet=UTF-8");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
